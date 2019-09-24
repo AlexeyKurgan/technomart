5099d4a352f1b3090fa5f323ad3562b0f0e3ccc3
Alexey Kurgan <alexkurgan19927@gmail.com>
2019-09-24 17:02:33 +0300

    add files slick slider and did top category menu in project

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {     
+  
+    $('.carousel__inner').slick({
+      //infinite: true, //прокрутка по кругу
+      autoplay: true,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    });
+      
+    });
