687f657d60e9b5fde214767d2351e4bd19cf3b7b
Alexey Kurgan <alexkurgan19927@gmail.com>
2019-09-25 12:18:22 +0300

    modified css and img files

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,5 @@
-$(document).ready(function() {     
-  
-    $('.carousel__inner').slick({
-      //infinite: true, //прокрутка по кругу
-      autoplay: true,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    });
-      
-    });
+$(document).ready(function(){
+  $('.category-menu__slider').slick({
+    infinite:true
+  });
+});
