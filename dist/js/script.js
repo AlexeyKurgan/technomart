f04edf3829a8dfbd71563e28ca1299193aa6b083
Alexey Kurgan <alexkurgan19927@gmail.com>
2019-09-25 15:30:36 +0300

    did category menu section in project

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  $('.category-menu__slider').slick();
+  $('.category-menu__slider').slick({
+    infinite:true,
+    dots:true,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='icons/icon-left.svg'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../images/shoe_story/arrow-right.png'>"
+
+  });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   $('.category-menu__slider').slick({
-    infinite:true
+    infinite:true,
+    dots:true,
+    autoplay:true,
+    autoplaySpeed:3000,
+    prevArrow:"<svg class='a-left control-c prev slick-prev'  src='../icons/icon-left.svg'></svg>",
+    nextArrow:"<svg class='a-right control-c next slick-next' src='../icons/icon-right.svg'></svg>"
   });
 });
