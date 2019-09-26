1cf386a7977139e2cd70eb146d03a6d4a4315ba2
Alexey Kurgan <alexkurgan19927@gmail.com>
2019-09-26 15:20:24 +0300

    added  new sections 'Popular goods and Popular brands' in project

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,8 +2,9 @@ $(document).ready(function(){
   $('.category-menu__slider').slick({
     infinite:true,
     dots:true,
-    prevArrow:"<img class='a-left control-c prev slick-prev' src='icons/icon-left.svg'>",
-    nextArrow:"<img class='a-right control-c next slick-next' src='../images/shoe_story/arrow-right.png'>"
-
+    autoplay:true,
+    autoplaySpeed:3000,
+    prevArrow:"<svg class='a-left control-c prev slick-prev'  src='../icons/icon-left.svg'></svg>",
+    nextArrow:"<svg class='a-right control-c next slick-next' src='../icons/icon-right.svg'></svg>"
   });
 });
